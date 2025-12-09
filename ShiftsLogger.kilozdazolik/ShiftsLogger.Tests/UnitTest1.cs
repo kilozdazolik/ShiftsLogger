2b9446a0feb67473c166403c89f93b0f2be191fb
kilozdazolik <paulsonzoltan@protonmail.com>
2025-12-09 11:15:24 +0100

    feat: merge solutions, tests project init

---
+++ ShiftsLogger.kilozdazolik/ShiftsLogger.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace ShiftsLogger.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ ShiftsLogger.kilozdazolik/ShiftsLogger.UI/Program.cs
@@ -0,0 +1,10 @@
+﻿using ShiftsLogger.UI;
+using ShiftsLogger.UI.Controllers;
+using ShiftsLogger.UI.Services;
+
+HttpClient client = new HttpClient();
+ShiftService shiftService = new ShiftService(client);
+ShiftController shiftController = new ShiftController(shiftService);
+UserInterface ui = new UserInterface(shiftController);
+
+await ui.MainMenu();
